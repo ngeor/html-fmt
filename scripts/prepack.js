3e31a8d22db11a627d7c84df3d49488cf2eb835b
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2021-05-12 08:56:47 +0200

    trying to copy .npmignore to out folder

--- scripts/prepack.js
+++ scripts/prepack.js
@@ -1,6 +1,11 @@
 const fs = require("fs");
 
 fs.copyFileSync("../../README.md", "out/README.md");
+
+if (fs.existsSync(".npmignore")) {
+    fs.copyFileSync(".npmignore", "out/.npmignore");
+}
+
 let packageJson = JSON.parse(fs.readFileSync("package.json", { encoding: "utf8"}));
 packageJson.main = "index.js";
 packageJson.scripts = {};
