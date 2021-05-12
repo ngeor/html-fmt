273d7f26b7a1c4ba42c16b3bd85240e4b0a0521b
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2021-05-12 08:44:11 +0200

    fix prepack script

--- scripts/prepack.js
+++ scripts/prepack.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.copyFileSync("../../README.md", "out/");
+fs.copyFileSync("../../README.md", "out/README.md");
 let packageJson = JSON.parse(fs.readFileSync("package.json", { encoding: "utf8"}));
 packageJson.main = "index.js";
 packageJson.scripts = {};
